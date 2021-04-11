8168781ee69e1eff5a3a684e051f8c90257a2274
iwenking <201468291@qq.com>
2021-04-11 15:15:14 +0800

    项目初始化

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ["module-resolver", {
+      root: ["./src"],
+      alias: {
+        "@/utils": "./src/utils",
+        "@/pages": "./src/pages",
+        "@/navigator": "./src/navigator",
+        "@/models": "./src/models",
+        "@/config": "./src/config",
+        "@/componets": "./src/componets",
+        "@/assets": "./src/assets",
+      }
+    }]
+  ]
+};
